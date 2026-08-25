435d9d66ed8e21145ad0b3a69cac447d7d88c6eb
Jay Tailor <155796776+heyjaytailor@users.noreply.github.com>
2026-08-25 16:41:42 +0530

    Add files via upload

---
+++ Models/FtpConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPUtility.Models
+{
+    public class FtpConfig
+    {
+        public string Host { get; set; }
+
+        public int Port { get; set; } = 21;
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public bool UseSsl { get; set; } = true;
+
+        public bool UsePassive { get; set; } = true;
+
+        public bool ValidateAnyCertificate { get; set; } = false;
+    }
+}
---
+++ Models/FtpResult.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPUtility.Models
+{
+    public class FtpResult
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public string ErrorCode { get; set; }
+
+        public List<string> Data { get; set; }
+
+        public static FtpResult Ok(string message)
+        {
+            return new FtpResult
+            {
+                Success = true,
+                Message = message
+            };
+        }
+
+        public static FtpResult Fail(
+            string errorMessage,
+            string errorCode = null)
+        {
+            return new FtpResult
+            {
+                Success = false,
+                Message = "FTP operation failed.",
+                ErrorMessage = errorMessage,
+                ErrorCode = errorCode
+            };
+        }
+    }
+}
